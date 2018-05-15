947160960a6ef8b2bd14788889b6954795ff389d
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-15 17:14:23 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,19 +1,24 @@
 class Artist
-  attr_accessor :name
-  @@songs = []
+  attr_accessor :name, :songs
+  @@all = []
 
   def initialize(name)
     @name = name
+    @songs = []
+    save
   end
 
-  def songs(song)
-    @@songs
+  def add_song(song)
+    song.artist = self
+    @songs << song
   end
 
-  def find_or_create_by_name(name)
-    if self.artist(name).uniq
-      self.artist.name = name
-    end 
+  def self.find_or_create_by_name(artist_name)
+    if all.detect {|specific_artist| specific_artist.name == artist_name}
+      artist_name
+    else
+      specific_artist = Artist.new(artist_name)
+    end
   end
 
   def save(song)
