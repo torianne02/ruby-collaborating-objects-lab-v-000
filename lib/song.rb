44094164c8086aed31afaab95934a458149b66db
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-15 16:57:29 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,10 @@
 class Song
   attr_accessor :name
 
+  def initialize(name)
+    @name = name
+  end
+
   def self.new_by_filename(filename)
     song = self.new
     song.name = filename.split(" - ")[1]
