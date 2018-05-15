6205aacc08617cfb7bc77e83d12b932cd132c017
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-15 17:40:22 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,15 +1,12 @@
 class MP3Importer
-  attr_accessor :path
+  attr_accessor :path, :files
 
   def initialize(path)
     @path = path
-  end
-
-  def files
-    Dir.entries(path).reject {|f| f == '.' || f == '..'}
+    @files = Dir.entries(path).grep(/.*\.mp3/)
   end
 
   def import
-    files.each {|filename| Song.new_by_filename(filename)}
+    @files.each {|filename| Song.new_by_filename(filename)}
   end
 end
