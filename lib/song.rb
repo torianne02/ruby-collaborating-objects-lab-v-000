e5a18c5b0f188c1ddd8e48b7f1e6e09b44e7eaae
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-15 17:42:53 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,14 +1,13 @@
 class Song
   attr_accessor :name, :artist
 
-  def initialize(name, artist = nil)
+  def initialize(name)
     @name = name
-    @artist = artist
   end
 
-  def self.new_by_filename(file)
-    song.name = file.chomp(".mp3").split(" - ")[1]
-    song = self.new
+  def self.new_by_filename(filename)
+    song_name = filename.split(" - ")[1]
+    song = self.new(song_name)
   end
 
   def artist_name=(name)
